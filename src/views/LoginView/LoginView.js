5c6a4da695625034d829b2d6fdd68702cc315a8f
abcbbxhdhd <ajax76@protonmail.com>
2022-04-03 00:02:15 +0300

    Added bussiness logic for register

--- src/views/LoginView/LoginView.js
+++ src/views/LoginView/LoginView.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import React, { useState } from 'react'
+import Button from '../../components/Button/Button'
+import Form from "../../components/Form/Form"
+import Password from '../../components/Password/Password'
+import { auth } from '../../firebase-config'
+import Email from '../../components/Email/Email'
+import FormTitle from '../../components/FormTitle/FormTitle'
+import FormLowerLabel from '../../components/FormLowerLabel/FormLowerLabel'
 
 function LoginView() {
+    const [input, setInput] = useState({
+        email: "",
+        password: ""
+    })
+
+    const onChange = (e) => {
+        const {name, value} = e.target
+        setInput(prev => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+    
+    const onClick = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, input.email, input.password)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
-        <></>
+        <Form>
+            <FormTitle type="login" />
+            <Email onChange={onChange} value={input.email} />
+            <Password onChange={onChange} value={input.password} />
+            <Button type="login" onClick={onClick} />
+            <FormLowerLabel type="login" />
+        </Form>
     )
 }
 
