5a3296ea07ebea2e13fe5b4b4f3dfbdb27332a1e
abcbbxhdhd <ajax76@protonmail.com>
2022-04-03 00:03:05 +0300

    Added bussiness logic for register

--- src/views/RegisterView/RegisterView.js
+++ src/views/RegisterView/RegisterView.js
@@ -1,8 +1,50 @@
-import React from "react"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import React, { useState } from "react"
+import Button from "../../components/Button/Button"
+import Email from "../../components/Email/Email"
+import Form from "../../components/Form/Form"
+import FormLowerLabel from "../../components/FormLowerLabel/FormLowerLabel"
+import FormTitle from "../../components/FormTitle/FormTitle"
+import Password from "../../components/Password/Password"
+import { auth } from "../../firebase-config"
 
 function RegisterView() {
+    const [input, setInput] = useState({
+        email: "",
+        password: "",
+        confirmation: ""
+    })
+
+    const onChange = (e) => {
+        const {name, value} = e.target
+        setInput(prev => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+    const onClick = async () => {
+        try {
+            if (input.password !== input.confirmation) {
+                throw new Error('Password not confirmed')
+            }
+            await createUserWithEmailAndPassword(auth, input.email, input.password)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
-        <></>
+        <Form>
+            <FormTitle type="register" />
+            <Email value={input.email} onChange={onChange}/>
+            <Password value={input.password} onChange={onChange}/>
+            <Password confirmation value={input.confirmation} onChange={onChange}/>
+            <Button type="register" onClick={onClick} />
+            <FormLowerLabel type="register" />
+        </Form>
     )
 }
 
